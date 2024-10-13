589116cee14325d2e32b91ffd4cf3096c82e2a88
Alp830 <alpsaruhan96@gmail.com>
2024-10-13 15:35:08 -0700

    Update BlindPeopleApp.py

--- BlindPeopleApp.py
+++ BlindPeopleApp.py
@@ -4,7 +4,6 @@ import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
 from gtts import gTTS
 import pyttsx3
-import comtypes.client 
 
 
 
@@ -84,7 +83,7 @@ if x:
     responses = generate_multiple_llm_responses(prompt)
     
     st.write(responses[0])
-    engine = pyttsx3.init('sapi5')
+    engine = pyttsx3.init()
 
     rate = engine.getProperty('rate')
     engine.setProperty('rate', 130)
